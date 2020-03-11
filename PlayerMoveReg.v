609fa15dbcb0fff8b932c04788a8fe731b0c93c0
Simar-B <tucker.bassi@mail.utoronto.ca>
2020-03-10 20:17:17 -0400

    Complete PlayerMoveReg.v

--- PlayerMoveReg.v
+++ PlayerMoveReg.v
@@ -6,16 +6,39 @@ module playerRegister(
      * Let 11 Represent LEFT
      */ 
     input CLOCK_50,
-    input [1:0] directionIN,
+    input [1:0] directionIN,directionCURRENT,
     output reg [1:0] directionOUT
     );
-    reg directionCURR;
-    initial
-        directionCURR = 2'b00;
     always(posedge CLOCK_50) begin
         case(directionIN)
             2'b00: begin
-                if()
-
+                if(directionCURRENT == 2'b10)
+                    directionOUT <= directionCURRENT
+                else
+                    directionOUT <= directionIN
             end
+
+            2'b01: begin
+                if(directionCURRENT == 2'b11)
+                    directionOUT <= directionCURRENT
+                else
+                    directionOUT <= directionIN
+            end
+
+            2'b10: begin
+                if(directionCURRENT == 2'b00)
+                    directionOUT <= directionCURRENT
+                else
+                    directionOUT <= directionIN
+            end
+
+            2'b11: begin
+                if(directionCURRENT == 2'b01)
+                    directionOUT <= directionCURRENT
+                else
+                    directionOUT <= directionIN
+            end
+        endcase
     end
+    
+endmodule
