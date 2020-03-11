180d48db62333269aa04ea9ab96bb1fd467427cf
Manav Patel <manav.patel@mail.utoronto.ca>
2020-03-10 23:01:53 -0400

    Update PlayerMoveReg.v
    Changed direction labelling

--- PlayerMoveReg.v
+++ PlayerMoveReg.v
@@ -2,8 +2,8 @@ module playerRegister(
 
     /* Let 00 Represent UP
      * Let 01 Represent RIGHT
-     * Let 10 Represent DOWN
-     * Let 11 Represent LEFT
+     * Let 11 Represent DOWN
+     * Let 10 Represent LEFT
      */ 
     input CLOCK_50,
     input [1:0] directionIN,directionCURRENT,
@@ -16,28 +16,28 @@ module playerRegister(
     always(posedge CLOCK_50) begin
         case(directionIN)
             2'b00: begin
-                if(directionCURRENT == 2'b10)
-                    directionOUT <= directionCURRENT
-                else
-                    directionOUT <= directionIN
-            end
-
-            2'b01: begin
                 if(directionCURRENT == 2'b11)
                     directionOUT <= directionCURRENT
                 else
                     directionOUT <= directionIN
             end
 
+            2'b01: begin
+                if(directionCURRENT == 2'b10)
+                    directionOUT <= directionCURRENT
+                else
+                    directionOUT <= directionIN
+            end
+
             2'b10: begin
-                if(directionCURRENT == 2'b00)
+                if(directionCURRENT == 2'b01)
                     directionOUT <= directionCURRENT
                 else
                     directionOUT <= directionIN
             end
 
             2'b11: begin
-                if(directionCURRENT == 2'b01)
+                if(directionCURRENT == 2'b00)
                     directionOUT <= directionCURRENT
                 else
                     directionOUT <= directionIN
